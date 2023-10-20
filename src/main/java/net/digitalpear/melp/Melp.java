407250ec22b13bfb0bad99faceafde95aa89bccb
DP <digitalpeargaming@gmail.com>
2023-10-19 22:25:06 -0700

    melp can no longer be obtained in the end

--- src/main/java/net/digitalpear/melp/Melp.java
+++ src/main/java/net/digitalpear/melp/Melp.java
@@ -3,9 +3,15 @@ package net.digitalpear.melp;
 import net.digitalpear.melp.init.MBlocks;
 import net.digitalpear.melp.init.MItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModification;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.VegetationPatchFeature;
+import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 
 public class Melp implements ModInitializer {
 
@@ -20,9 +26,6 @@ public class Melp implements ModInitializer {
                 if (id == LootTables.SNIFFER_DIGGING_GAMEPLAY){
                     tableBuilder.modifyPools(builder -> builder.with(ItemEntry.builder(MItems.MELP_SEED)));
                 }
-//                else if (id == LootTables.END_CITY_TREASURE_CHEST){
-//                    tableBuilder.pool(LootPool.builder().with(ItemEntry.builder(MItems.MELP_SEED).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 3.0f))).conditionally(RandomChanceLootCondition.builder(0.3f))).build());
-//                }
             }
         });
     }
